bc0c29bd943928b45b953091713101f88b2e29a6
Laska <klimovskih.laska@mail.ru>
2022-12-11 20:17:17 +0700

    дз 3 из 3

---
+++ HM_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// запишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine ("Введите число ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int r = 0;
+while (number != 0)
+{
+    r = number % 10;
+    number = number / 10;
+    sum = sum + r; 
+}
+
+Console.WriteLine("сумма чисел "+ sum);
---
+++ HM_3/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int[] randArray = new int[8];
+
+for (int i = 0; i < randArray.Length; i++)
+{
+    randArray[i] = new Random().Next(1,9);
+    Console.Write(randArray[i] + " ");
+}
+
+
+
